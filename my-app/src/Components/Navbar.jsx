c09a0d59e8ee7bb7d5a95353580d2a350930f5d0
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2022-11-10 23:33:07 +0530

    day3 file added

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,32 +1,26 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import LoginDrawer from './LoginDrawer'
-import {CartIcon,OffersIcon,AppIcon,LogoIcon} from "./AllIcon"
-import { Box,Flex,Image } from '@chakra-ui/react'
+import { CartIcon, OffersIcon, AppIcon, LogoIcon } from "./AllIcon"
+import { Box, Flex, Image } from '@chakra-ui/react'
 
 
 function Navbar() {
 
   return (
-    <Box 
-    display="flex" alignItems="center" justifyContent="space-around"
-    boxShadow='md' p='0' rounded='md' bg='white'>
+    
+    <Box
+      display="flex" alignItems="center" justifyContent="space-around"
+      boxShadow='md' p='0' rounded='md' bg='white'>
       <Box className='LogoContainer'>
-        <Link to="/"><LogoIcon  /> </Link>
-
+        <Link to="/"><LogoIcon /> </Link>
       </Box>
-      <Box display="flex" alignItems="center" justifyContent="space-between" gap="2.5rem">
 
-      <Link to="/appdownload">
-        <AppIcon/>
-        Download App</Link>
-      <Link to="/login"><LoginDrawer/></Link>
-      <Link to="/offers">
-        <OffersIcon/>Offers
-      </Link>
-      <Link to="/cart">
-        <CartIcon/> Cart
-        </Link>
+      <Box display="flex" alignItems="center" justifyContent="space-between" gap="2.5rem">
+        <Link to="/appdownload"><AppIcon />Download App</Link>
+        <Link to="/login"><LoginDrawer /></Link>
+        <Link to="/offers"><OffersIcon />Offers</Link>
+        <Link to="/cart"><CartIcon /> Cart</Link>
       </Box>
     </Box>
   )
--- my-app/src/Routes/Home.jsx
+++ my-app/src/Routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {SearchBar} from "../Components/SearchBar"
 import {Category} from "../Components/Category"
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>Home
       <SearchBar/>
-      <Category/>
+      
+      <Link to="/healthcare"><Category/></Link>
     </div>
   )
 }
